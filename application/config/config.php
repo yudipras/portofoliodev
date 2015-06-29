d7c297d63386ab0bfa63f729a323aa4fdd57e940
Yudi Prasetyo <yudiprasetyo@hotmail.com>
2015-06-29 22:01:50 +0700

    perubahan di macem2 view

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$config['base_url'] = 'http://portofolio.dev/';
+$config['base_url'] = 'http://localhost/portofoliodev';
 
 /*
 |--------------------------------------------------------------------------
